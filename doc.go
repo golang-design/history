0578d4e5c2c185d95f8745bfdf878ff3f20fc1cf
Changkun Ou <hi@changkun.us>
2020-09-18 19:41:05 +0200

    public: use overflow-wrap for mobile device

--- doc.go
+++ doc.go
@@ -71,7 +71,7 @@ const indexTemplate = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Go: A Documentary</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
