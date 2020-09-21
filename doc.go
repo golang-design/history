5c2f48851c58d013712eff7ddceb4fad222043bb
Changkun Ou <hi@changkun.us>
2020-09-21 08:40:30 +0200

    public: add google analytics

--- doc.go
+++ doc.go
@@ -80,6 +80,14 @@ const indexTemplate = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-80889616-4"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-80889616-4');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Go: A Documentary</title>
